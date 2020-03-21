92eae98a80adf77817e401e9db852862696e0566
jacekwozniak12 <jac3kw0zniak@gmail.com>
2020-03-21 16:18:36 +0100

    Adding functionality -WIP

--- app/tictactoe.js
+++ app/tictactoe.js
@@ -1,27 +1,39 @@
 var Board = /** @class */ (function () {
     function Board() {
+        this.cells = new Array();
         this.movesAmount = 0;
+        this.playerTurn = Math.random() + 1;
     }
     Board.prototype.Draw = function (htmlElement) {
         var element_GameBoard = document.querySelector(htmlElement);
         for (var i = 0; i < 9; i++) {
-            var el = document.createElement("p");
+            var el = document.createElement("div");
             el.className = "cell cell" + i;
+            var c = new Cell;
+            c.content = "";
+            c.representation = el;
+            el.addEventListener("click", this.SelectCell.bind(this));
+            el.addEventListener("click", c.DrawContent.bind(this));
+            this.cells.push(c);
             element_GameBoard.appendChild(el);
         }
+    };
+    Board.prototype.SelectCell = function () {
+        this.CheckIfFinished();
+        this.CurrentPlayer();
     };
     Board.prototype.CurrentPlayer = function () {
         this.playerTurn != 1 ? 1 : 2;
     };
-    Board.prototype.SelectCell = function () {
-        //
-    };
     Board.prototype.CheckIfFinished = function () {
-        if (this.movesAmount > 8)
-            return true;
+        if (this.movesAmount > 8) {
+            this.DisplayDraw();
+        }
         else {
             return false;
         }
+    };
+    Board.prototype.DisplayDraw = function () {
     };
     return Board;
 }());
@@ -29,11 +41,17 @@ var Cell = /** @class */ (function () {
     function Cell() {
     }
     Cell.prototype.DrawContent = function () {
-        this.representation.innerHTML == '';
+        this.representation.innerHTML == 't';
     };
     return Cell;
 }());
 var element_Score_P1 = document.querySelector("player1-score");
 var element_Score_P2 = document.querySelector("player2-score");
-var gameBoard = new Board;
-gameBoard.Draw("#game-board");
+function GameHandler() {
+    var gameBoard = new Board;
+    gameBoard.Draw("#game-board");
+}
+function ScoreHandler() {
+}
+GameHandler();
+//# sourceMappingURL=tictactoe.js.map
--- src/tictactoe.ts
+++ src/tictactoe.ts
@@ -1,31 +1,54 @@
 class Board{  
 
-    cells: Cell[];
+    cells: Cell[] = new Array();
     playerTurn: number;
-    movesAmount = 0;
+    winner: number;
+    movesAmount : number = 0;
+
+    constructor(){
+        this.playerTurn = Math.random() + 1;
+    }
 
     Draw(htmlElement: string){
+        
         const element_GameBoard = document.querySelector(htmlElement);
+        
         for(let i = 0; i < 9; i++){
-            let el = document.createElement("p");
+
+            let el = document.createElement("div");
             el.className = "cell cell" + i;
+            let c = new Cell;
+            c.content = "";
+            c.representation = el;
+            el.addEventListener("click", this.SelectCell.bind(this));
+            el.addEventListener("click", c.DrawContent.bind(c));
+            this.cells.push(c);
+
             element_GameBoard.appendChild(el);
         }
     }
 
-    CurrentPlayer(){
-        this.playerTurn != 1 ? 1 : 2
+    SelectCell(){    
+        this.CheckIfFinished();
+        this.CurrentPlayer();
     }
 
-    SelectCell(){
-        //
+    CurrentPlayer(){
+        this.playerTurn != 1 ? 1 : 2
+        this.movesAmount++;
     }
 
     CheckIfFinished() : boolean{
-        if(this.movesAmount > 8) return true;
+        if(this.movesAmount > 8){
+            this.DisplayDraw();
+        }
         else{
             return false
         }
+    }
+
+    DisplayDraw(){
+        alert("Draw");
     }
 }
 
@@ -35,14 +58,21 @@ class Cell{
     representation: HTMLElement;
 
     DrawContent(){
-        this.representation.innerHTML == '';
+        this.representation.innerHTML == 't';
     }
-
 
 }
 
 const element_Score_P1 = document.querySelector("player1-score");
 const element_Score_P2 = document.querySelector("player2-score");
 
-const gameBoard = new Board;
-gameBoard.Draw("#game-board");
+function GameHandler(){
+    const gameBoard = new Board;
+    gameBoard.Draw("#game-board");
+}
+
+function ScoreHandler(){
+
+}
+
+GameHandler();
