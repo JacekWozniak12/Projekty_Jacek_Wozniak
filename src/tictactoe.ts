ac59cef877c3218b9917c0d727d2480c7a22c138
jacekwozniak12 <jac3kw0zniak@gmail.com>
2020-03-21 15:14:18 +0100

    Methods preps, html structure

---
+++ src/tictactoe.ts
@@ -0,0 +1,36 @@
+class Board{  
+
+    cells: Cell[];
+
+    Draw(){
+        
+    }
+
+    CurrentPlayer(){
+
+    }
+
+    SelectCell(){
+
+    }
+
+    CheckIfFinished(){
+
+    }
+}
+
+class Cell{
+
+    content: string;
+
+    DrawContent(){
+
+    }
+
+
+}
+
+
+const element_GameBoard = document.querySelector("game-board");
+const element_Score_P1 = document.querySelector("player1-score");
+const element_Score_P2 = document.querySelector("player2-score");
