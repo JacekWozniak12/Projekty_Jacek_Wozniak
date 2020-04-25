3455cebd796b77b3c1d8ad0f991763208503122b
jacekwozniak12 <jac3kw0zniak@gmail.com>
2020-04-25 09:30:58 +0200

    wip

--- app/tictactoe.js
+++ app/tictactoe.js
@@ -2,15 +2,16 @@ var element_P1 = document.querySelector("#player1");
 var element_P2 = document.querySelector("#player2");
 var element_Score_P1 = document.querySelector("player1-score");
 var element_Score_P2 = document.querySelector("player2-score");
+var P1 = 'x';
+var P2 = 'o';
+var P1Score = 0;
+var P2Score = 0;
 var Board = /** @class */ (function () {
     function Board() {
         this.NUMBER_OF_MOVES = 9;
         this.cells = new Array();
         this.movesAmount = 1;
-        this.playerTurn = Math.random() + 1;
-        this.playerTurn == 1 ?
-            Setup(element_P1, element_P2) :
-            Setup(element_P2, element_P1);
+        this.Start();
     }
     Board.prototype.Draw = function (htmlElement) {
         var element_GameBoard = document.querySelector(htmlElement);
@@ -18,54 +19,129 @@ var Board = /** @class */ (function () {
             this.CreateCell(i, element_GameBoard);
         }
     };
+    Board.prototype.Start = function () {
+        this.SelectPlayer();
+    };
+    Board.prototype.SelectPlayer = function () {
+        this.playerTurn = Math.round(Math.random()) + 1;
+        this.playerTurn == 1 ?
+            Setup(element_P1, element_P2) :
+            Setup(element_P2, element_P1);
+    };
     Board.prototype.CreateCell = function (i, element_GameBoard) {
         var el = document.createElement("div");
+        var c = new Cell(el, this);
         el.className = "cell cell" + i;
-        var c = new Cell;
         c.content = "";
         c.representation = el;
-        el.addEventListener("click", this.SelectCell.bind(this));
-        el.addEventListener("click", c.DrawContent.bind(c));
+        var binding = c.SelectedCell.bind(c);
+        el.addEventListener("click", binding, true);
         this.cells.push(c);
         element_GameBoard.appendChild(el);
     };
-    Board.prototype.SelectCell = function () {
-        this.CheckIfFinished();
-        this.CurrentPlayer();
+    Board.prototype.CellWasSelected = function () {
+        if (!this.CheckIfFinished()) {
+            this.CurrentPlayer();
+        }
     };
     Board.prototype.CurrentPlayer = function () {
         this.playerTurn != 1 ? this.playerTurn = 1 : this.playerTurn = 2;
         this.movesAmount++;
         if (this.playerTurn == 1) {
-            ChangePlayer(element_P1, element_P2);
+            SelectDeselect(element_P1, element_P2);
         }
         else {
-            ChangePlayer(element_P2, element_P1);
+            SelectDeselect(element_P2, element_P1);
         }
     };
     Board.prototype.CheckIfFinished = function () {
         if (this.movesAmount >= this.NUMBER_OF_MOVES) {
             this.DisplayDraw();
+            return true;
         }
         else {
-            return false;
+            if (this.CheckIfWon(this.playerTurn)) {
+                return true;
+            }
+            else
+                return false;
         }
+    };
+    Board.prototype.DisplayWin = function () {
+        alert("Player " + this.playerTurn + " won");
+        this.Reset();
     };
     Board.prototype.DisplayDraw = function () {
         alert("Draw");
         this.Reset();
     };
     Board.prototype.Reset = function () {
-        this.movesAmount = 0;
-        this.playerTurn = Math.random() + 1;
+        this.Start();
+        this.Draw("#game-board");
+    };
+    Board.prototype.CheckIfWon = function (player) {
+        var checkFor;
+        if (this.playerTurn == 1)
+            checkFor = P1;
+        else
+            checkFor = P2;
+        // will do cleaner way, for now would suffice
+        if (this.cells[0].content = checkFor,
+            this.cells[1].content = checkFor,
+            this.cells[2].content = checkFor) {
+            return true;
+        }
+        if (this.cells[0].content = checkFor,
+            this.cells[3].content = checkFor,
+            this.cells[6].content = checkFor) {
+            return true;
+        }
+        if (this.cells[0].content = checkFor,
+            this.cells[4].content = checkFor,
+            this.cells[8].content = checkFor) {
+            return true;
+        }
+        if (this.cells[1].content = checkFor,
+            this.cells[4].content = checkFor,
+            this.cells[7].content = checkFor) {
+            return true;
+        }
+        if (this.cells[2].content = checkFor,
+            this.cells[5].content = checkFor,
+            this.cells[8].content = checkFor) {
+            return true;
+        }
+        if (this.cells[3].content = checkFor,
+            this.cells[4].content = checkFor,
+            this.cells[5].content = checkFor) {
+            return true;
+        }
+        if (this.cells[6].content = checkFor,
+            this.cells[7].content = checkFor,
+            this.cells[8].content = checkFor) {
+            return true;
+        }
+        return false;
     };
     return Board;
 }());
 var Cell = /** @class */ (function () {
-    function Cell() {
+    function Cell(representation, board) {
+        this.board = board;
+        this.content = "";
+        this.representation = representation;
     }
-    Cell.prototype.DrawContent = function () {
-        this.representation.innerHTML == 't';
+    Cell.prototype.SelectedCell = function () {
+        if (this.board.playerTurn == 1) {
+            this.content = P1;
+            this.representation.innerHTML = P1;
+        }
+        else if (this.board.playerTurn == 2) {
+            this.content = P2;
+            this.representation.innerHTML = P2;
+        }
+        this.representation.click = null;
+        this.board.CellWasSelected();
     };
     return Cell;
 }());
@@ -75,9 +151,9 @@ function GameHandler() {
 }
 function ScoreHandler() {
 }
-function ChangePlayer(previous, current) {
-    Deselect(previous);
+function SelectDeselect(current, previous) {
     Select(current);
+    Deselect(previous);
 }
 function Select(element) {
     element.classList.replace("inactive", "active");
--- src/tictactoe.ts
+++ src/tictactoe.ts
@@ -3,21 +3,24 @@ const element_P2 = document.querySelector("#player2");
 const element_Score_P1 = document.querySelector("player1-score");
 const element_Score_P2 = document.querySelector("player2-score");
 
+const P1 : string = 'x';
+const P2 : string = 'o';
+let P1Score = 0;
+let P2Score = 0;
+
 class Board{  
 
     NUMBER_OF_MOVES : number = 9;
 
     cells: Cell[] = new Array();
     playerTurn: number;
-    winner: number;
     movesAmount : number = 1;
 
     Draw(htmlElement: string){
         
         const element_GameBoard = document.querySelector(htmlElement);
-        
-        for(let i = 0; i < 9; i++){
 
+        for(let i = 0; i < 9; i++){
             this.CreateCell(i, element_GameBoard);
         }
     }
@@ -27,27 +30,36 @@ class Board{
     }
 
     private Start() {
-        this.playerTurn = Math.random() + 1;
+        this.SelectPlayer();
+    }
+
+    private SelectPlayer() {
+        this.playerTurn = Math.round(Math.random()) + 1;
         this.playerTurn == 1 ?
-            Setup(<HTMLDivElement>element_P1, <HTMLDivElement>element_P2):
+            Setup(<HTMLDivElement>element_P1, <HTMLDivElement>element_P2) :
             Setup(<HTMLDivElement>element_P2, <HTMLDivElement>element_P1);
     }
 
     private CreateCell(i: number, element_GameBoard: Element) {
         let el = document.createElement("div");
+        let c = new Cell(el, this);
+
         el.className = "cell cell" + i;
-        let c = new Cell;
+
         c.content = "";
         c.representation = el;
-        el.addEventListener("click", this.SelectCell.bind(this));
-        el.addEventListener("click", c.DrawContent.bind(c));
+
+        let binding = c.SelectedCell.bind(c);
+        el.addEventListener("click", binding, true);
+        
         this.cells.push(c);
         element_GameBoard.appendChild(el);
     }
 
-    SelectCell(){    
-        this.CheckIfFinished();
-        this.CurrentPlayer();
+    CellWasSelected() {    
+        if (!this.CheckIfFinished()){
+            this.CurrentPlayer(); 
+        }
     }
 
     CurrentPlayer(){
@@ -55,13 +67,13 @@ class Board{
         this.movesAmount++;
         
         if(this.playerTurn == 1){
-            ChangePlayer(
+            SelectDeselect(
                 <HTMLDivElement> element_P1, 
                 <HTMLDivElement> element_P2
                 );
         }
         else{
-            ChangePlayer(
+            SelectDeselect(
                 <HTMLDivElement> element_P2, 
                 <HTMLDivElement> element_P1
             );
@@ -71,10 +83,20 @@ class Board{
     CheckIfFinished() : boolean{
         if(this.movesAmount >= this.NUMBER_OF_MOVES){
             this.DisplayDraw();
+            return true;
         }
         else{
-            return false
+            if(this.CheckIfWon(this.playerTurn)){
+                return true;
+            }
+            else return false
         }
+    }
+
+    DisplayWin(){
+        alert("Player "+this.playerTurn+" won");
+
+        this.Reset();
     }
 
     DisplayDraw(){
@@ -84,18 +106,82 @@ class Board{
 
     private Reset() {
         this.Start();
+        this.Draw("#game-board");
+    }
+
+    CheckIfWon(player : number) : boolean{
+        let checkFor : string;
+        if(this.playerTurn == 1) checkFor = P1; 
+        else checkFor = P2;
+
+        // will do cleaner way, for now would suffice
+        if (this.cells[0].content = checkFor,
+            this.cells[1].content = checkFor,
+            this.cells[2].content = checkFor) {
+            return true;
+        }
+
+        if (this.cells[0].content = checkFor,
+            this.cells[3].content = checkFor,
+            this.cells[6].content = checkFor) {
+            return true;
+        }
+        if (this.cells[0].content = checkFor,
+            this.cells[4].content = checkFor,
+            this.cells[8].content = checkFor) {
+            return true;
+        }
+        if (this.cells[1].content = checkFor,
+            this.cells[4].content = checkFor,
+            this.cells[7].content = checkFor) {
+            return true;
+        }
+        if (this.cells[2].content = checkFor,
+            this.cells[5].content = checkFor,
+            this.cells[8].content = checkFor) {
+            return true;
+        }
+        if (this.cells[3].content = checkFor,
+            this.cells[4].content = checkFor,
+            this.cells[5].content = checkFor) {
+            return true;
+        }
+        if (this.cells[6].content = checkFor,
+            this.cells[7].content = checkFor,
+            this.cells[8].content = checkFor) {
+            return true;
+        }
+        return false;
+        
     }
 }
 
 class Cell{
 
+    board : Board;
     content: string;
-    representation: HTMLElement;
+    representation: HTMLDivElement;
 
-    DrawContent(){
-        this.representation.innerHTML == 't';
+    constructor(representation : HTMLDivElement, board : Board){
+        this.board = board;
+        this.content = "";
+        this.representation = representation;
     }
 
+    SelectedCell(){
+
+            if(this.board.playerTurn == 1){
+                this.content = P1;
+                this.representation.innerHTML = P1;
+            }
+            else if(this.board.playerTurn == 2) {
+                this.content = P2;
+                this.representation.innerHTML = P2;
+            }
+    
+            this.representation.click = null;
+            this.board.CellWasSelected();
+    }
 }
 
 function GameHandler(){
@@ -107,9 +193,9 @@ function ScoreHandler(){
 
 }
 
-function ChangePlayer(previous : HTMLDivElement, current : HTMLDivElement ){
-    Deselect(previous);
+function SelectDeselect(current : HTMLDivElement, previous : HTMLDivElement ){
     Select(current);
+    Deselect(previous);
 }
 
 function Select(element : HTMLDivElement){
